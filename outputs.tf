d98aac6b0ecbb1c2cf1ed2836fef367f8e901295
Scott Brimhall <scott@brimhallmail.com>
2021-03-09 15:59:25 -0800

    Add count

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "instance" {
-  value = {
-    metadata     = maas_instance.main
-    ip_addresses = var.enable_google_dns == true ? google_dns_record_set.main.rrdatas : nil
-  }
+  count = length(maas_instance.main)
+  value = [
+    {
+      metadata     = maas_instance.main[count.index]
+      ip_addresses = var.enable_google_dns == true ? google_dns_record_set.main[count.index].rrdatas : nil
+    }
+  ]
 }
