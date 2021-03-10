fffd3ae5e18c4bf3b87e6f227e45bf187898a856
Scott Brimhall <scott@brimhallmail.com>
2021-03-10 10:06:14 -0800

    Revert "Try that"
    This reverts commit 4cd0fc5f21bc58e86363d7b76caa330e8b7c7c8c.

--- instance.tf
+++ instance.tf
@@ -6,6 +6,7 @@ resource "maas_instance" "main" {
   release_erase = var.release_erase
   install_kvm   = var.install_kvm
   ip_addresses  = var.hosts[count.index]["ip_addresses"]
+  address       = var.hosts[count.index]["ip_addresses"][0]
 
   lifecycle {
     ignore_changes = [user_data]
