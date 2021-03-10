6081ddd99cd2dcce7789c2173c8e5de21e0e651e
Scott Brimhall <scott@brimhallmail.com>
2021-03-10 13:20:54 -0800

    Use id

--- instance.tf
+++ instance.tf
@@ -14,7 +14,7 @@ resource "maas_instance" "main" {
 
 resource "maas_interface_physical" "main" {
   count       = length(var.hosts)
-  system_id   = maas_instance.main[count.index].system_id
+  system_id   = maas_instance.main[count.index].id
   mac_address = var.hosts[count.index]["mac_address"]
   vlan        = var.vlan
   depends_on  = [maas_instance.main]
@@ -22,8 +22,8 @@ resource "maas_interface_physical" "main" {
 
 resource "maas_interface_link" "main" {
   count        = length(var.hosts)
-  system_id    = maas_interface_physical.main[count.index].system_id
-  interface_id = maas_interface_physical.main[count.index].interface_id
+  system_id    = maas_instance.main[count.index].id
+  interface_id = maas_interface_physical.main[count.index].id
   subnet_id    = var.subnet_id
   mode         = "STATIC"
   ip_address   = var.hosts[count.index]["ip_addresses"][0]
