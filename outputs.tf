4450d1f5258dac2b58235cf89dc43c665d1c59a4
Scott Brimhall <scott@brimhallmail.com>
2021-03-10 10:45:34 -0800

    Just put the first one

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "instance_metadata" {
-  value = resource.maas_instance.main
+  value = resource.maas_instance.main[0]
 }
 
 output "interfaces" {
   value = [
-    resource.maas_interface_physical.main,
-    resource.maas_interface_link.main
+    resource.maas_interface_physical.main[0],
+    resource.maas_interface_link.main[0]
   ]
 }
