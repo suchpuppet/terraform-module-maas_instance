0125701072b5fb50086eff4a47e2ede7767db862
Scott Brimhall <scott@brimhallmail.com>
2021-03-10 13:23:08 -0800

    Remove interfaces

--- instance.tf
+++ instance.tf
@@ -11,21 +11,3 @@ resource "maas_instance" "main" {
     ignore_changes = [user_data]
   }
 }
-
-resource "maas_interface_physical" "main" {
-  count       = length(var.hosts)
-  system_id   = maas_instance.main[count.index].id
-  mac_address = var.hosts[count.index]["mac_address"]
-  vlan        = var.vlan
-  depends_on  = [maas_instance.main]
-}
-
-resource "maas_interface_link" "main" {
-  count        = length(var.hosts)
-  system_id    = maas_instance.main[count.index].id
-  interface_id = maas_interface_physical.main[count.index].id
-  subnet_id    = var.subnet_id
-  mode         = "STATIC"
-  ip_address   = var.hosts[count.index]["ip_addresses"][0]
-  depends_on   = [maas_interface_physical.main]
-}
