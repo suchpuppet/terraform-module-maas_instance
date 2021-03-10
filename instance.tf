c353acf5bd02b1d8899a9b5b96bf3b336b6df411
Scott Brimhall <scott@brimhallmail.com>
2021-03-10 15:03:57 -0800

    Fix distro

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "maas_instance" "main" {
   count         = length(var.hosts)
   hostname      = var.hosts[count.index]["hostname"]
   user_data     = var.user_data == "" ? data.template_file.cloudinit[count.index].rendered : var.user_data
-  distro_series = var.hosts[count.index]["distro"] != nil ? var.hosts[count.index]["distro"] : var.distro
+  distro_series = var.hosts[count.index]["distro"] == "" ? var.distro : var.hosts[count.index]["distro"]
   release_erase = var.release_erase
   install_kvm   = var.install_kvm
   ip_addresses  = var.hosts[count.index]["ip_addresses"]
