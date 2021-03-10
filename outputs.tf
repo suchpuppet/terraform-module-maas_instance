51232b5e754cc131b4851cc1054d37ed4c583138
Scott Brimhall <scott@brimhallmail.com>
2021-03-09 16:00:57 -0800

    Remove outputs for now

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance" {
-  count = length(maas_instance.main)
-  value = [
-    {
-      metadata     = maas_instance.main[count.index]
-      ip_addresses = var.enable_google_dns == true ? google_dns_record_set.main[count.index].rrdatas : nil
-    }
-  ]
-}
