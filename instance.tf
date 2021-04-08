97a7db09ec47a37c055021d74d502d4ea0e87e85
Scott Brimhall <scott@brimhallmail.com>
2021-04-08 13:32:50 -0700

    Add user data

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "maas_instance" "main" {
   count         = length(var.hosts)
   hostname      = var.hosts[count.index]["hostname"]
-  # user_data     = var.user_data == "" ? data.template_file.cloudinit[count.index].rendered : var.user_data
+  user_data     = var.user_data == "" ? data.template_file.cloudinit[count.index].rendered : var.user_data
   distro_series = var.distro
   release_erase = var.release_erase
   install_kvm   = var.install_kvm
